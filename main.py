4fa7e5bcb6f63874b749e8b415c882f63d09a51e
Eduardo Berlatto <eduardoberlatto@gmail.com>
2024-04-17 16:00:42 -0300

    funçao minImage e maxImage criadas, falta teste

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from PIL import ImageTk, Image, ImageEnhance, ImageFilter
 from tkinter import filedialog, messagebox
 from pathlib import Path
+import cv2
 
 # definindo propriedades do front-end
 root = Tk()
@@ -34,8 +35,8 @@ def openimage():
     img_no = img_no + 1
     filename[img_no] = "img.png"
     img = Image.open(filename[img_no])
-    img = ImageTk.PhotoImage(img)
     #front-end
+    img = ImageTk.PhotoImage(img)
     panel = Label(root, image=img)
     panel.image = img
     panel.place(x=150, y=40)
@@ -55,10 +56,41 @@ def grayscaleimage():
     img_no = img_no + 1
     color.enhance(0).save(str(img_no)+'.png')
     filename[img_no] = str(img_no)+'.png'
+    #front-end
+    img = ImageTk.PhotoImage(filename[img_no])
+    panel = Label(root, image=filename[img_no])
+    panel.image = filename[img_no]
+    panel.place(x=150, y=40)
+    updateimage()
+
+def MinImage():
+    global filename, img_no
+    img = Image.open(filename[img_no])
+    width, height = img.size
+    newHeight = int (height*0.5)
+    newWidth = int (width*0.5)
+    imageMin = img.resize((newWidth,newHeight), Image.BILINEAR)
+    imageMin.save("imagemmin.png")
+    img_no = img_no + 1
+    filename[img_no] = ("imagemin.png")
+    updateimage()
+
+def MaxImage():
+    global filename, img_no
+    img = Image.open(filename[img_no])
+    width, height = img.size
+    newHeight = int (height*2)
+    newWidth = int (width*2)
+    imageMin = img.resize((newWidth,newHeight), Image.BILINEAR)
+    imageMin.save("imagemmax.png")
+    img_no = img_no + 1
+    filename[img_no] = ("imagemax.png")
     updateimage()
 
 Button(root, text='Abrir Imagem', height="1", width="15", bg="#04BF68", fg="#ffffff", bd="0", cursor="hand2", font="Montserrat", command=openimage).place(x=2, y=2)
 Button(root, text='Escala de Cinza', height="1", width="15", bg="#04BF68", fg="#ffffff", bd="0", cursor="hand2", font="Montserrat", command=grayscaleimage).place(x=2, y=350)
-
+Button(root, text='Rotacionar', height="1", width="15", bg="#04BF68", fg="#ffffff", bd="0", cursor="hand2", font="Montserrat", command=rotateimage).place(x=2, y=400)
+Button(root, text='Minimizar', height="1", width="15", bg="#04BF68", fg="#ffffff", bd="0", cursor="hand2", font="Montserrat", command=MinImage).place(x=2, y=450)
+Button(root, text='Maximizar', height="1", width="15", bg="#04BF68", fg="#ffffff", bd="0", cursor="hand2", font="Montserrat", command=MaxImage).place(x=2, y=500)
 
 root.mainloop()
