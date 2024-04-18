c773d369629d480d25d392a63330f0ba39ba58a4
Eduardo Berlatto <eduardoberlatto@gmail.com>
2024-04-18 16:22:45 -0300

    implementado downloadimage, consertado o label de imagem e o updateimage

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from PIL import ImageTk, Image, ImageEnhance, ImageFilter
 from tkinter import filedialog, messagebox
 from pathlib import Path
+import shutil
 import cv2
 
 # definindo propriedades do front-end
@@ -48,6 +49,20 @@ def updateimage():
     img_enhance = ImageTk.PhotoImage(img_enhance)
     panel = Label(root, image=img_enhance)
     panel.image = img_enhance
+    panel.place(x=150, y=40)
+
+def downloadimage():
+    global filename, img_no
+    # Verificando se a foto teve alterações
+    if filename[img_no] != "img.png":
+        try:
+            # Alterar o caminho da pasta download de acordo com seu PC
+            shutil.move(filename[img_no], "C:/Users/Berlatto/Downloads/" + filename[img_no])
+            messagebox.showinfo("Sucesso!", "Imagem baixada com sucesso! Verifique sua pasta de downloads.")
+        except Exception as e: 
+            messagebox.showerror("Erro", f"Erro ao baixar a imagem: {e}")
+    else:
+        messagebox.showinfo("Inalterada", "Nenhuma imagem para baixar.")
 
 def grayscaleimage():
     global filename, img_no
@@ -56,11 +71,6 @@ def grayscaleimage():
     img_no = img_no + 1
     color.enhance(0).save(str(img_no)+'.png')
     filename[img_no] = str(img_no)+'.png'
-    #front-end
-    img = ImageTk.PhotoImage(filename[img_no])
-    panel = Label(root, image=filename[img_no])
-    panel.image = filename[img_no]
-    panel.place(x=150, y=40)
     updateimage()
 
 def rotateimage():
@@ -101,5 +111,6 @@ Button(root, text='Escala de Cinza', height="1", width="15", bg="#04BF68", fg="#
 Button(root, text='Rotacionar', height="1", width="15", bg="#04BF68", fg="#ffffff", bd="0", cursor="hand2", font="Montserrat", command=rotateimage).place(x=2, y=400)
 Button(root, text='Minimizar', height="1", width="15", bg="#04BF68", fg="#ffffff", bd="0", cursor="hand2", font="Montserrat", command=MinImage).place(x=2, y=450)
 Button(root, text='Maximizar', height="1", width="15", bg="#04BF68", fg="#ffffff", bd="0", cursor="hand2", font="Montserrat", command=MaxImage).place(x=2, y=500)
+Button(root, text='Download', height="1", width="15", bg="#04BF68", fg="#ffffff", bd="0", cursor="hand2", font="Montserrat", command=downloadimage).place(x=400, y=650)
 
 root.mainloop()
